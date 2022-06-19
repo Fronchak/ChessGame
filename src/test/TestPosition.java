fffda9bca22576f91e3aa366250af9b211a94aec
Gabriel Fronchak <gabrielgmack@gmail.com>
2022-06-18 22:58:45 -0300

    Create class Position

---
+++ src/test/TestPosition.java
@@ -0,0 +1,14 @@
+package test;
+
+import boardgame.Position;
+
+public class TestPosition {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(2, 5);
+		System.out.println(position);
+		
+	}
+
+}
